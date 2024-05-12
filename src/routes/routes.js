3c60f67d374f3b89693624acfe5e2d2e649447f2
hiskandar <hiskandaryapis@gmail.com>
2024-05-12 22:05:10 +0700

    middleware revised

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,8 +8,8 @@ const { authenticateToken } = require("../middleware/jsonwebtoken");
 // Define routes
 router.post("/register", register);
 router.post("/login", logIn);
-router.get("/user", authenticateToken, getUser); 
-router.get("/realtime", authenticateToken, getRealtime); 
-router.get("/records", authenticateToken, getRecords); 
+router.get("/user", getUser); 
+router.get("/realtime", getRealtime); 
+router.get("/records", getRecords); 
 
 module.exports = router;
