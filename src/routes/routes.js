e3c77afa634bb4ed3e7395e2745e840e582e0c3d
hiskandar <hiskandaryapis@gmail.com>
2024-05-15 18:32:38 +0700

    jwt removed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,17 @@
 //routes.js
 const express = require("express");
 const router = express.Router();
-const { register, logIn, getUser, getRealtime, getRecords } = require("../handler/handler");
+const { register, logIn, getUser, getRealtime, getRecords, getControl, putControl } = require("../handler/handler");
 const { authenticateToken } = require("../middleware/jsonwebtoken");
 
 
 // Define routes
-router.post("/register", register);
-router.post("/login", logIn);
-router.get("/user", authenticateToken, getUser); 
-router.get("/realtime", authenticateToken, getRealtime); 
-router.get("/records", authenticateToken, getRecords); 
+router.post("/user", register);
+router.post("/user/login", logIn);
+router.get("/user", getUser); 
+router.get("/realtime", getRealtime); 
+router.get("/records", getRecords); 
+router.get("/control", getControl); 
+router.put("/control", putControl); 
 
 module.exports = router;
