afc178a55bcfd34792a29a6a8c36eabe7474d57a
hiskandar <hiskandaryapis@gmail.com>
2024-08-09 22:47:59 +0700

    token removed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,13 +6,15 @@ const { register,
     getUser, 
     getRealtime, 
     postRealtime, 
+    postRecords,
     getRecords, 
     getControl, 
     putControlTemp, 
     putControlMoist, 
     deactivateDevice, 
     activateDevice, 
-    getState } = require("../handler/handler");
+    getState,
+    getDays } = require("../handler/handler");
 const { authenticateToken } = require("../middleware/jsonwebtoken");
 
 
@@ -22,12 +24,14 @@ router.post("/user/login", logIn);
 router.get("/user", authenticateToken, getUser); 
 router.get("/realtime", getRealtime); 
 router.post("/realtime", postRealtime);
+router.post("/records", postRecords); 
 router.get("/records", getRecords); 
 router.get("/control", getControl); 
-router.put("/control/temperatur", authenticateToken, putControlTemp); 
-router.put("/control/kelembapan", authenticateToken, putControlMoist); 
-router.put("/state/activate", authenticateToken, activateDevice);
-router.put("/state/deactivate", authenticateToken, deactivateDevice);
+router.put("/control/temperature", authenticateToken, putControlTemp); 
+router.put("/control/moisture", authenticateToken, putControlMoist); 
+router.put("/state/activate", activateDevice);
+router.put("/state/deactivate", deactivateDevice);
 router.get("/state", authenticateToken, getState);
+router.get("/time/days", authenticateToken, getDays);
 
 module.exports = router;
